58cccde33a450a49d0887b4e7f15a0d0363a5e4a
Jade <jade2.eames@live.uwe.ac.uk>
2023-02-06 13:48:14 +0000

    Camera Manager done

--- Assets/Scenes/Assets/Camera Manager/CameraManager.cs
+++ Assets/Scenes/Assets/Camera Manager/CameraManager.cs
@@ -7,20 +7,34 @@ public class CameraManager : MonoBehaviour
 {
     public float move_speed = 1;
     public float zoom_sensitivity = 1;
+    public float shift_click_zoom_step = 5.0f;
 
     private bool top_down_view = false;
+    private float cam_down_tilt;
+
     private Camera cam;
+    private GameObject cam_rig;
     private InputAction move_input;
     private InputAction zoom_input;
 
     private void Awake()
     {
+        // TO BE REMOVED
+        Cursor.visible = true;
+        
         cam = GetComponent<Camera>();
+        cam_rig = transform.parent.gameObject;
+        cam_down_tilt = cam.transform.localRotation.eulerAngles.x;
     }
 
     private void Start()
     {
         InputManager.Instance.my_input_actions.AfterLifeActions.ToggleTopDownView.started += toggleTopDownView;
+        InputManager.Instance.my_input_actions.AfterLifeActions.CenterANDZoomIn.started += centerIn;
+        InputManager.Instance.my_input_actions.AfterLifeActions.CenterANDZoomOut.started += centerOut;
+        InputManager.Instance.my_input_actions.AfterLifeActions.JumpViewToHeaven.started += jumpToHeaven;
+        InputManager.Instance.my_input_actions.AfterLifeActions.JumpViewToHell.started += jumpToHell;
+
         move_input = InputManager.Instance.my_input_actions.AfterLifeActions.MoveView;
         zoom_input = InputManager.Instance.my_input_actions.AfterLifeActions.Zoom;
     }
@@ -31,36 +45,64 @@ public class CameraManager : MonoBehaviour
 
         if (!top_down_view)
         {
-            transform.Rotate(transform.right, -90);
+            cam.transform.Rotate(transform.right, -90 + cam_down_tilt);
         }
         else
         {
-            transform.Rotate(transform.right, 90);
+            cam.transform.Rotate(transform.right, 90 - cam_down_tilt);
         }
     }
 
     private void FixedUpdate()
     {
-        Vector3 move_direction = new Vector3(move_input.ReadValue<Vector2>().x * move_speed, move_input.ReadValue<Vector2>().y * move_speed, 0);
-        Vector3 mouse_world_pos = cam.ScreenToWorldPoint(Input.mousePosition);
-        mouse_world_pos.z = cam.transform.position.z - 100;
+        moveView(move_input.ReadValue<Vector2>());
+        ZoomView(zoom_input.ReadValue<Single>());
+    }
 
-        if (!top_down_view)
+    private void moveView(Vector2 move_vector)
+    {
+        if (move_vector.magnitude != 0.0)
         {
-            move_direction = Quaternion.FromToRotation(Vector3.forward, transform.forward) * move_direction;
-            mouse_world_pos = Quaternion.FromToRotation(Vector3.forward, transform.forward) * mouse_world_pos;
+            Vector3 move_direction = new Vector3(move_vector.x * move_speed, move_vector.y * move_speed, 0);
+            if (top_down_view)
+            {
+                move_direction = Quaternion.FromToRotation(Vector3.up, cam_rig.transform.forward) * move_direction;
+            }
+            cam_rig.transform.position += move_direction;
         }
-        else
-        {
-            move_direction = Quaternion.FromToRotation(Vector3.up, transform.up) * move_direction;
-            mouse_world_pos = Quaternion.FromToRotation(Vector3.up, transform.up) * mouse_world_pos;
-        }
+    }
 
-        transform.position += move_direction;
+    private void ZoomView(float zoom_step)
+    {
+        if (zoom_step != 0)
+        {
+            cam_rig.transform.position += zoom_step * zoom_sensitivity * cam.transform.forward;
+        }
+    }
+
+    private void centerIn(InputAction.CallbackContext context)
+    {
+        Vector3 cursor_world_pos = cam.ScreenToWorldPoint(Input.mousePosition);
+        cam_rig.transform.position = new Vector3(cursor_world_pos.x, cursor_world_pos.y, cam_rig.transform.position.z);
         
-        if (zoom_input.ReadValue<Single>() != 0)
-        {
-            transform.position += mouse_world_pos;
-        }
+        ZoomView(shift_click_zoom_step);
+    }
+
+    private void centerOut(InputAction.CallbackContext context)
+    {
+        Vector3 cursor_world_pos = cam.ScreenToWorldPoint(Input.mousePosition);
+        cam_rig.transform.position = new Vector3(cursor_world_pos.x, cursor_world_pos.y, cam_rig.transform.position.z);
+        
+        ZoomView(-shift_click_zoom_step);
+    }
+
+    private void jumpToHell(InputAction.CallbackContext context)
+    {
+        // get position of hell from Game Manager and set cam_rig position on x & y to it, z to a comfortable distance.
+    }
+
+    private void jumpToHeaven(InputAction.CallbackContext context)
+    {
+        // get position of heaven from Game Manager and set cam_rig position on x & y to it, z to a comfortable distance.
     }
 }
